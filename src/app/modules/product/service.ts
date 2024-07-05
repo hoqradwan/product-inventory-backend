0f924b6c8b727e181b20b0f15352b882d88f4b05
ahmad musa abdullah <ahmadmusa9805@gmail.com>
2024-07-05 16:05:46 +0600

    complete creating  product api

--- src/app/modules/product/service.ts
+++ src/app/modules/product/service.ts
@@ -1,7 +1,10 @@
 import { TProduct } from "./interface";
 import { ProductModel } from "./model";
 
-const createProductIntoDB = async (createProduct: TProduct) => {};
+const createProductIntoDB = async (createProduct: TProduct) => {
+  const result = await ProductModel.create(createProduct);
+  return result;
+};
 const getProductsFromDB = async () => {};
 const getProductFromDB = async (id: string) => {};
 const updateProductFromDB = async (
