9bb302527336f02d24933815a0045ba9629c74de
ahmad musa abdullah <ahmadmusa9805@gmail.com>
2024-07-05 21:08:45 +0600

    complete route not found apo

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 // import express, { Application, Request, Response } from "express";
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import { productRoutes } from "./app/modules/product/route";
 import { ordersRoutes } from "./app/modules/order/route";
+// import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 const app: Application = express();
 
+//parser
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
 
@@ -16,5 +19,13 @@ app.use("/api/orders", ordersRoutes);
 //   console.log(req.body);
 //   res.send(a);
 // });
+
+// 404 Error Handler
+app.use((req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+  });
+});
 
 export default app;
